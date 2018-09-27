f41b9e18d4b89c8e2cb8b2477c644188629dd41a
Jirateep <jirateeptobua@gmail.com>
2018-09-27 15:11:25 +0700

    asf
    sfafsa

---
+++ main.php
@@ -0,0 +1,400 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>RESUME Jirateep tobua</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Mitr" rel="stylesheet"> 
+    
+</head>
+
+<body>
+ <!-- Links -->
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-black font-weight-bold fixed-top scrolling-navbar" id="nav">
+    <div class="container">
+        <!-- Navbar brand -->
+        <a class="navbar-brand text-white" id="res">RESUME</a>
+
+        <div class="collapse navbar-collapse" id="basicExampleNav">
+            <!-- Links -->
+            <ul class="navbar-nav mr-auto smooth-scroll">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#nav">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#cs">Detail</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#work">Work</a>
+                </li>
+            </ul>         
+        </div>
+        <!-- Collapsible content -->
+    </div>
+</nav>
+
+
+<div align="center" class="cs">
+<img src="RESUME_Jirateep.jpg"  class="wow fadeIn z-depth-5" id="resume" >
+</div>
+
+<!-- Section: Blog v.1 -->
+<div class="phot">
+<section class="my-5">
+
+  <!-- Section heading -->
+  <h2 class="h1-responsive text-white font-weight-bold text-center my-5">ผลงาน</h2>
+  <!-- Section description -->
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-lg-5">
+
+      <!-- Featured image -->
+      <div class="wow slideInLeft view overlay rounded z-depth-2 mb-lg-0 mb-4">
+        <img class="img-fluid " src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg" alt="Sample image">
+        <a>
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-7">
+
+      <!-- Category -->
+      <a href="#!" class="green-text"><h6 class="font-weight-bold mb-3"><i class="fa fa-cutlery pr-2"></i>Food</h6></a>
+      <!-- Post title -->
+      <h3 class="font-weight-bold mb-3"><strong>Title of the news</strong></h3>
+      <!-- Excerpt -->
+      <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus et aut officiis debitis.</p>
+      <!-- Post data -->
+      <p>by <a><strong>Carine Fox</strong></a>, 19/08/2018</p>
+      <!-- Read more button -->
+      <a class="btn btn-success btn-md">Read more</a>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+  <hr class="my-5">
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-lg-7">
+
+      <!-- Category -->
+      <a href="#!" class="pink-text"><h6 class="font-weight-bold mb-3"><i class="fa fa-image pr-2"></i>Lifestyle</h6></a>
+      <!-- Post title -->
+      <h3 class="font-weight-bold mb-3"><strong>Title of the news</strong></h3>
+      <!-- Excerpt -->
+      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+      <!-- Post data -->
+      <p>by <a><strong>Carine Fox</strong></a>, 14/08/2018</p>
+      <!-- Read more button -->
+      <a class="btn btn-pink btn-md mb-lg-0 mb-4">Read more</a>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-5">
+
+      <!-- Featured image -->
+      <div class="wow slideInRight view overlay rounded z-depth-2">
+        <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/img%20(34).jpg" alt="Sample image">
+        <a>
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+  <hr class="my-5">
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-lg-5">
+
+      <!-- Featured image -->
+      <div class="wow slideInLeft view overlay rounded z-depth-2 mb-lg-0 mb-4">
+        <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/img (28).jpg" alt="Sample image">
+        <a>
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-7">
+     <!-- Category -->
+      </i>Travels</h6></a>
+      <!-- Post title -->
+      <h3 class="font-weight-bold mb-3"><strong>Title of the news</strong></h3>
+
+      <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro qui dolorem ipsum quia sit amet.</p>
+
+      <p>by <a><strong>Carine Fox</strong></a>, 11/08/2018</p>
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</section>
+</div>
+<!-- Section: Blog v.1 -->
+<!--/.Navbar-->
+<!-- Links -->
+   
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    
+    <script>
+	wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+	})
+	wow.init();
+	</script>
+    
+</body>
+
+=======
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>RESUME Jirateep tobua</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Mitr" rel="stylesheet"> 
+    
+</head>
+
+<body>
+ <!-- Links -->
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-black font-weight-bold fixed-top scrolling-navbar" id="nav">
+    <div class="container">
+        <!-- Navbar brand -->
+        <a class="navbar-brand text-white" id="res">RESUME</a>
+
+        <div class="collapse navbar-collapse" id="basicExampleNav">
+            <!-- Links -->
+            <ul class="navbar-nav mr-auto smooth-scroll">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#nav">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#cs">Detail</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#work">Work</a>
+                </li>
+            </ul>         
+        </div>
+        <!-- Collapsible content -->
+    </div>
+</nav>
+
+
+<div align="center" class="cs">
+<img src="RESUME_Jirateep.jpg"  class="wow fadeIn z-depth-5" id="resume" >
+</div>
+
+<!-- Section: Blog v.1 -->
+<div class="phot">
+<section class="my-5">
+
+  <!-- Section heading -->
+  <h2 class="h1-responsive text-white font-weight-bold text-center my-5">ผลงาน</h2>
+  <!-- Section description -->
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-lg-5">
+
+      <!-- Featured image -->
+      <div class="wow slideInLeft view overlay rounded z-depth-2 mb-lg-0 mb-4">
+        <img class="img-fluid " src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg" alt="Sample image">
+        <a>
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-7">
+
+      <!-- Category -->
+      <a href="#!" class="green-text"><h6 class="font-weight-bold mb-3"><i class="fa fa-cutlery pr-2"></i>Food</h6></a>
+      <!-- Post title -->
+      <h3 class="font-weight-bold mb-3"><strong>Title of the news</strong></h3>
+      <!-- Excerpt -->
+      <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus et aut officiis debitis.</p>
+      <!-- Post data -->
+      <p>by <a><strong>Carine Fox</strong></a>, 19/08/2018</p>
+      <!-- Read more button -->
+      <a class="btn btn-success btn-md">Read more</a>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+  <hr class="my-5">
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-lg-7">
+
+      <!-- Category -->
+      <a href="#!" class="pink-text"><h6 class="font-weight-bold mb-3"><i class="fa fa-image pr-2"></i>Lifestyle</h6></a>
+      <!-- Post title -->
+      <h3 class="font-weight-bold mb-3"><strong>Title of the news</strong></h3>
+      <!-- Excerpt -->
+      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+      <!-- Post data -->
+      <p>by <a><strong>Carine Fox</strong></a>, 14/08/2018</p>
+      <!-- Read more button -->
+      <a class="btn btn-pink btn-md mb-lg-0 mb-4">Read more</a>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-5">
+
+      <!-- Featured image -->
+      <div class="wow slideInRight view overlay rounded z-depth-2">
+        <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/img%20(34).jpg" alt="Sample image">
+        <a>
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+  <hr class="my-5">
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-lg-5">
+
+      <!-- Featured image -->
+      <div class="wow slideInLeft view overlay rounded z-depth-2 mb-lg-0 mb-4">
+        <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/img (28).jpg" alt="Sample image">
+        <a>
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-7">
+     <!-- Category -->
+      </i>Travels</h6></a>
+      <!-- Post title -->
+      <h3 class="font-weight-bold mb-3"><strong>Title of the news</strong></h3>
+
+      <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro qui dolorem ipsum quia sit amet.</p>
+
+      <p>by <a><strong>Carine Fox</strong></a>, 11/08/2018</p>
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</section>
+</div>
+<!-- Section: Blog v.1 -->
+<!--/.Navbar-->
+<!-- Links -->
+   
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    
+    <script>
+	wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+	})
+	wow.init();
+	</script>
+    
+</body>
+
+>>>>>>> 33930ac976bebc371da48be5491a405cf09123af
+</html>
